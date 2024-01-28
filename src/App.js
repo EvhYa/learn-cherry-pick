3fdec6137568eb42f964f0dff35def53178de48b
EvhYa <nicetea.bro@gmail.com>
2024-01-28 11:00:26 +0200

    add new component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { NewComp } from "./component/newcomp";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
         <div>test main branch</div>
+        <NewComp />
       </header>
     </div>
   );
